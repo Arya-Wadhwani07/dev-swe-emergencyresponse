1618888fe5ceed2c526a57c4297e7858840e5837
Arya Wadhwani <arya.wadhwani@gmail.com>
2022-09-13 23:08:27 +0530

    Add User Schema:

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,33 @@
 const { Sequelize } = require('sequelize')
 const database = require("../secrets")
 
-const sequelize = new Sequelize(database.DATABASE_NAME, database.DATABASE_USERNAME, database.DATABASE_PASSWORD,{
+const sequelize = new Sequelize(
+    database.DATABASE_NAME, 
+    database.DATABASE_USERNAME, 
+    database.DATABASE_PASSWORD, {
     host: database.DATABASE_HOST,
-    dialect: database.DATABASE_DIALECT
+    dialect: database.DATABASE_DIALECT,
+    sync:{
+        force:true
+    }
 })
 
-try{
-    sequelize.authenticate()
-    console.log("Authentication Successfull")
-} catch{
-    console.log("Error")
+const models = {
+    user: sequelize.import("../models/user"),
 }
+
+try {
+    sequelize.authenticate().then(()=>{
+        console.log("Authentication Successfull")
+    })
+    sequelize.sync({ force: true, logging: true }).then(()=>{
+        console.log("Synchronised Successfully")
+    })
+} catch(e) {
+    console.log(e)
+}
+
+models.sequelize = sequelize
+models.Sequelize = Sequelize
+
+module.exports = models
---
+++ src/models/user.js
@@ -0,0 +1,59 @@
+const validator = require('validator')
+module.exports = (sequelize, DataTypes)=>{
+    const user = sequelize.define('user',{
+        id:{
+            type: DataTypes.UUID,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                checkName(value){
+                    if(!(validator.isAlpha(value))){
+                        throw new Error("Invalid Name!")
+                    }
+                }
+            }
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                checkEmail(value){
+                    if(!(validator.isEmail(value))){
+                        throw new Error("Invalid Email!")
+                    }
+                }
+            }   
+        },
+        contactNumber:{
+            type: DataTypes.STRING,
+            unique: true,
+            validate:{
+                checkPhoneNum(value){
+                    if(!validator.isMobilePhone(value)){
+                        throw new Error("Invalid Phone Number!")
+                    }
+                }
+            }
+        },
+        address:{
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        dateOfBirth:{
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        bloodGroup:{
+            type: DataTypes.ENUM("A+","B+","O+","AB+","A-","B-","O-","AB-"),
+            allowNull: false
+        }
+    })
+    return user
+}
