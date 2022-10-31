b2594e73b4459125af4f97a0415ffa990f4850bb
Arya Wadhwani <arya.wadhwani@gmail.com>
2022-10-31 11:58:15 +0530

    Added User tests

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-const express = require('express')
-const cors = require('cors')
-const userRouter = require('./routers/user')
-const messageRouter = require("./routers/messages")
-const policeRouter = require("./routers/police")
-const emergencyContactInfoRouter = require("./routers/emergencyContactInfo")
-require('./db/db')
-
-
-const app = express()
+const app = require("./app")
 const port = 3000
-
-app.use(cors())
-app.use(express.json())
-app.use(userRouter)
-app.use(messageRouter)
-app.use(policeRouter)
-app.use(emergencyContactInfoRouter)
-
 
 app.listen(port, ()=>{
     console.log("Port Listening on "+port)
---
+++ src/tests/getUser.test.js
@@ -0,0 +1,41 @@
+const app = require("../app")
+const request = require("supertest")
+const token = require("./token")
+
+test("Get All the users",async()=>{
+    const res = await request(app).get('/user').send()
+    expect(res.status).toBe(200)
+})
+
+test("Create a User",async()=>{
+    const res = await request(app).post('/user').set('Authorization','Bearer '+token).send({
+        name:"Arya",
+        contactNumber:"7722043607",
+        address:"ABCD",
+        dateOfBirth: "07/08/2002",
+        bloodGroup:"A+"  
+    })
+    expect(res.status).toBe(200)
+})
+
+test("Checking with improper phone number",async()=>{
+    const res = await request(app).post('/user').set('Authorization','Bearer '+token).send({
+        name:"Arya",
+        contactNumber:"abcd1234",
+        address:"ABCD",
+        dateOfBirth: "07/08/2002",
+        bloodGroup:"A+" 
+    })
+    expect(res.status).toBe(400)
+})
+
+test("Checking with improper blood group",async()=>{
+    const res = await request(app).post('/user').set('Authorization','Bearer '+token).send({
+        name:"Arya",
+        contactNumber:"abcd1234",
+        address:"ABCD",
+        dateOfBirth: "07/08/2002",
+        bloodGroup:"C+" 
+    })
+    expect(res.status).toBe(400)
+})
