1defd558cf58f438c2242613f2a2b5d3117eb635
Arya Wadhwani <arya.wadhwani@gmail.com>
2022-10-31 12:00:16 +0530

    Added User tests

---
+++ src/secrets.js
@@ -0,0 +1,32 @@
+const database = {
+    DATABASE_NAME: "blrtweikqpvvzbf0hj3c",
+    DATABASE_USERNAME: "ud1l4uj6n18prukr",
+    DATABASE_PASSWORD: "HIIBf3UnQE5DVaehKbKn",
+    DATABASE_HOST: "blrtweikqpvvzbf0hj3c-mysql.services.clever-cloud.com",
+    DATABASE_DIALECT: "mysql"
+}
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4bX50s5gvY71LZCZHebqmGc_S-UQhT8A",
+    authDomain: "devswemergencyresponse.firebaseapp.com",
+    projectId: "devswemergencyresponse",
+    storageBucket: "devswemergencyresponse.appspot.com",
+    messagingSenderId: "498370827044",
+    appId: "1:498370827044:web:474bbfdb8b3419a7a599b1",
+    measurementId: "G-MK814HRKE9"
+}
+
+const firebaseAdminConfig = {
+    type: "service_account",
+    project_id: "devswemergencyresponse",
+    private_key_id: "b1e6af4c09e432ca4a0da40506a9ebf5c9235da9",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrkPhbJ8ZuR6/b\nXw8BRXfdSWy6idmxmjiwgscZuLuOl27MTu/h2LIPag/pC751CAJAu93YCN7sIbkJ\ns0nvqiYLQEgnlHdqh0qmjzp/ItgPUmSPUzEKO7+6g9uPYqhcRs4IdmxwRwnDebbl\nlaj7TpgDq+3mQsZAiGQzSTCEv38WnFrUGlb1sgLJXWTdxlv1hykEgRCilBuOB7Vw\nfcarcFaVtfmtw0CZmY/+iJ7CnhNdGNyA1nBbdxIMVORhcpLhVQJJQD7Fwe1n5HWL\nCxUC9ZgWvBhua5KZ/xOYmB+wd9krdwdUdefJoBwI+xPTa3+TPss9TxY5Ft4wEvfu\nGuXaXIdtAgMBAAECggEACk4SGM7mDIwvltEvcnUE0Cy/mYxmSr8NE3m7ce2mYwLG\nA9gO8QVmG+ZpOmcl7gYd39uuOse89Lke0eNsDWJD2Dj+IdXOqWj4ayeJt4a1LGXC\nHANpcT2pqbnkoh4RJ5UU0th4HB4Biwqgbsr4jtBc8ZrYq9QMGpFcAWJD8N6cCH8g\n8W5crs3ve38OVyq+ROoeb8HW3ufnKpnx6g9lYCBmfPJCwQk6NiDchw9nLjo1TEdI\n8gtGR51mc4eMG5ISEdtVyxltdHC65l0rADdA9Qd9X5MmQno5K5Pj4PrrkCpFwOzu\n/fBd4mcwp//mnM4LsGdxOBjKiZBn6IvrrucUC7ui6QKBgQDkdn0R8X3pH/3hvyos\nPgZdWUQmEegf3NNn//Jw69uQ3WoAshtXin23eGuEvcAgKptIOM+98thZcuzb12pP\nQXxWzRCFQLwDF05PrT4VYUK/iEaMafF3V7dDkUXvX5rUkpJD4P5N0XtuFEgTqTyX\n5zZTxRgTY264FjHCEuSQOgJplQKBgQDAPt20idtBnPA06EpmVbzPTBNbVuNEnGyX\n984flwwTIuXk/M5YcxaJECnHRXVK8eVSE8dl4yt7fQOJU1JmISlOsQzIIjrvAeLF\nm5RwwXc0mMP6Q12g20ut9Zc658ju6AyYu25Kmur/AhOCReuxZjBpPEkdP2qQSyHa\n8fZktG4geQKBgF4B3479lrjRoC8+8QSJdSb+L0uW/LP3ssnNiJ4sfwmKAi9NGnEY\nHIjdNSjGx4K05XZ/YgQgJCQSZSJE7oq0QIybDtw1+ecvtNqWR8CWZ9fWJGlswp4/\n6roT0fogld44fZUOxLWSqD5wfAQTFErRKgqoRDLeLFX1U/4utfwe0pVNAoGAAsQk\nzoc5R21pq2k4QNa0+8qdcLAEXyVdDqa7KYsz2kajRjM5CN7HETvRThO6x1RJt2+f\na+Dj7Ef1OBGh2uRZQs+bXnnXnT7Ks4s0jJbMD06vazdy7nCZWyxwe6GAK6fdHol7\n4cju3rJ582LsLEZLJgh/CNOzxdW+6/aVVAegmgkCgYEAj33asZWld+QRBCo5fm+l\nxGuB43zKjAFPGhsQJsN83/93RRcS52bvzLjCrUzZ/eO+CHAOn6Hb9wMx+kN0Tgfm\nZV5U+dz1sYHORhw9jyRIXLAzZmt+44y1pB6ODOfxi+X9P6vK5+UK4aGwAs3y2qaH\njoc9B8aP2QUT3+INaPAWndo=\n-----END PRIVATE KEY-----\n",
+    client_email: "firebase-adminsdk-h3pbq@devswemergencyresponse.iam.gserviceaccount.com",
+    client_id: "105049032098412548617",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-h3pbq%40devswemergencyresponse.iam.gserviceaccount.com"
+}
+
+module.exports = {database, firebaseConfig, firebaseAdminConfig}
