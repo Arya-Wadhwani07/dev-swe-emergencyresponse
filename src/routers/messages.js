cc4e0c35aeaee2fec0045f404ca041ce447a634d
Arya Wadhwani <arya.wadhwani@gmail.com>
2022-11-06 23:02:58 +0530

    added priority message

--- src/routers/messages.js
+++ src/routers/messages.js
@@ -5,50 +5,65 @@ const checkUser = require("../middleware/checkUser")
 
 const router = express.Router()
 
-router.get("/messages", async(req,res)=>{
-    try{
+router.get("/messages", async (req, res) => {
+    try {
         const messages = await models.messages.findAll()
         return res.status(200).send(messages)
-    } catch(e){
+    } catch (e) {
         return res.status(400).send(e)
     }
 })
 
-router.post("/messages",checkUser, async(req,res)=>{
-    try{
-        const user = await models.user.findAll({where:{email:req.user.email}})
+router.post("/messages", checkUser, async (req, res) => {
+    try {
+        const user = await models.user.findAll({ where: { email: req.user.email } })
         const messages = await models.messages.create({
             userId: user[0].dataValues.id,
             emergencyType: req.body.emergencyType,
             emergencyMessage: req.body.emergencyMessage,
             location: req.body.location,
-            extraInformation: req.body.extraInformation
+            extraInformation: req.body.extraInformation,
+            notified: "FALSE"
         })
         return res.status(200).send(messages)
-    } catch(e){
+    } catch (e) {
         return res.status(400).send(e)
     }
 })
 
-router.get("/priorityMessage", async(req,res)=>{
-    try{
+router.get("/priorityMessage", async (req, res) => {
+    try {
         var messagesArray = []
-        const messages = await models.findAll({where:{notified:"FALSE"}})
-        console.log(messages)
-        messages.forEach(message => {
+        var finale
+        var temp
+        var finale_data = []
+        const messages = await models.messages.findAll({ where: { notified: "FALSE" } })
+        Promise.all(messages.map(message => {
             messagesArray.push(message.emergencyMessage)
-        });
-        console.log(messagesArray)
-        const data = {
-            text:messagesArray
-        }
-        axios.post("http://127.0.0.1:5000",data).then(function (response) {
-            res.status(200).send(response)
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    } catch(e){
+        }))
+        const data = { text: messagesArray }
+        await axios.post("https://disaster-response-api-01.herokuapp.com", data)
+            .then(async (response) => {
+                temp = response.data
+                console.log(temp)
+                const final_data = await models.messages.findAll({
+                    where: {
+                        emergencyMessage: {
+                            in: temp
+                        }
+                    }
+                })
+                finale = final_data
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        Promise.all(temp.map((data, index) => {
+            var result = finale.find(item => item.emergencyMessage == data)
+            finale_data.push(result)
+        }))
+        res.status(200).send(finale_data)
+    } catch (e) {
         res.status(400).send(e)
     }
 })
