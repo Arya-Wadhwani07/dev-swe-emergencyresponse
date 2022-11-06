acabffbf2e8bee649611d992ae319677bb06f925
Arya Wadhwani <arya.wadhwani@gmail.com>
2022-11-06 12:51:04 +0530

    Added index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const app = require("./app")
+const express = require('express')
+const cors = require('cors')
+const userRouter = require('./routers/user')
+const messageRouter = require("./routers/messages")
+const policeRouter = require("./routers/police")
+const emergencyContactInfoRouter = require("./routers/emergencyContactInfo")
+require('./db/db')
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(userRouter)
+app.use(messageRouter)
+app.use(policeRouter)
+app.use(emergencyContactInfoRouter)
+
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
